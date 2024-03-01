21bae1f78ec5fc176dc553b513a005f469732da3
Viivi Kauppinen <hellothaovy@yahoo.com.vn>
2024-03-01 23:30:35 +0200

    connect React to electron

---
+++ mediadbms-app/src/components/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Home() {
+    return (
+        <div>
+            <h1>Blog Posts</h1>
+            <ul>
+                    <li>Hello from Component</li>
+            </ul>
+        </div>
+    );
+}
+
+export default Home;
