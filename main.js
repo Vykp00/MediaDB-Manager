ed317fa69dce153c0bf0720ac4055da4e3bd936e
Viivi Kauppinen <hellothaovy@yahoo.com.vn>
2024-03-03 23:10:24 +0200

    transaction management done

--- main.js
+++ main.js
@@ -51,7 +51,6 @@ const createWindow = () => {
 
 // Open the app window when it's ready
 app.whenReady().then(() => {
-  // TODO: Connect to PostgreSQL database using Client
   createWindow();
 
   // Open a window if none are open (macOS)
@@ -62,37 +61,11 @@ app.whenReady().then(() => {
   });
 });
 
-// FIXME: Refactor for Transactional Management.
-//    Change Pool to Client, and set custom query. Do not use them in parallel
-// Function to execute SQL query inside a transaction
-// // async function executeQueryInTransaction(query, ...params) {
-// //     try {
-// //         await pgClient.query('BEGIN'); // Start transaction
-// //         const result = await pgClient.query(query); // Execute query
-// //         await pgClient.query('COMMIT'); // Commit transaction
-// //         return result.rows; // Return query result
-// //     } catch (error) {
-// //         await pgClient.query('ROLLBACK'); // Rollback transaction on error
-// //         throw error;
-// //     }
-// // }
-
-// TODO: Refactor to match transaction
-//     // Handle IPC message from renderer process to execute SQL query
-//     ipcMain.on('executeQuery', async (event, query) => {
-//         try {
-//             const result = await executeQueryInTransaction(query);
-//             event.reply('queryResult', result);
-//         } catch (error) {
-//             console.error('Error executing query:', error);
-//             event.reply('queryError', error.message);
-//         }
-//     });
-
 // JOIN and aggregate functions to show most popular groups by posts
 ipcMain.on('getGroupReport', async (event) => {
+  const client = await pool.connect();
   try {
-    const client = await pool.connect();
+    await client.query('BEGIN'); // Start transaction
     const result = await client.query(
       'SELECT \n' +
       '    g.groupID,\n' +
@@ -113,76 +86,96 @@ ipcMain.on('getGroupReport', async (event) => {
       '    g.groupID, g.name\n' +
       'ORDER BY \n' +
       '    totalPosts DESC;\n'
-    );
+    ); // Execute query
     const dashBoard = result.rows;
-    event.reply('groupReportResult', dashBoard);
-    client.release();
-  } catch (err) {
-    console.error('Error executing query', err);
-    event.reply('groupReportResult', []);
+    await client.query('COMMIT'); // Commit transaction
+    event.reply('groupReportResult', dashBoard); // Send query result back to renderer process
+  } catch (error) {
+    await client.query('ROLLBACK'); // Rollback transaction on error
+    console.error('Error executing query:', error); // Send error message
+    event.reply('groupReportResult', []); // Send nothing back
+  } finally {
+    client.release(); // Release connection back to the pool
   }
 });
 
 // SELECT ALL Posts from post table
 ipcMain.on('getPosts', async (event) => {
+  const client = await pool.connect();
   try {
-    const client = await pool.connect();
+    await client.query('BEGIN'); // Start transaction
     const result = await client.query('SELECT * FROM post;');
     const posts = result.rows;
+    await client.query('COMMIT'); // Commit transaction
     event.reply('posts', posts);
-    client.release();
   } catch (err) {
-    console.error('Error executing query', err);
+    await client.query('ROLLBACK'); // Rollback transaction on error
+    console.error('Error executing query:', error); // Send error message
     event.reply('posts', []);
+  } finally {
+    client.release(); // Release connection back to the pool
   }
 });
 
 // Get all groups from group table
 ipcMain.on('getGroups', async (event) => {
+  const client = await pool.connect();
   try {
-    const client = await pool.connect();
+    await client.query('BEGIN'); // Start transaction
     const result = await client.query('SELECT * FROM "group";');
     const groups = result.rows;
+    await client.query('COMMIT'); // Commit transaction
     event.reply('groups', groups);
-    client.release();
   } catch (err) {
+    await client.query('ROLLBACK'); // Rollback transaction on error
     console.error('Error executing query', err);
     event.reply('groups', []);
+  } finally {
+    client.release(); // Release connection back to the pool
   }
 });
 
 // Get all user from user table
 ipcMain.on('getUsers', async (event) => {
+  const client = await pool.connect();
   try {
-    const client = await pool.connect();
+    await client.query('BEGIN'); // Start transaction
     const result = await client.query('SELECT * FROM "user";');
     const users = result.rows;
+    await client.query('COMMIT'); // Commit transaction
     event.reply('users', users);
-    client.release();
   } catch (err) {
+    await client.query('ROLLBACK'); // Rollback transaction on error
     console.error('Error executing query', err);
     event.reply('users', []);
+  } finally {
+    client.release();
   }
 });
 
 // Get all comments for a post
 ipcMain.on('getComments', async (event, postId) => {
+  const client = await pool.connect();
   try {
-    const client = await pool.connect();
+    await client.query('BEGIN'); // Start transaction
     const result = await client.query('SELECT * FROM comment WHERE postid = $1 ORDER BY commentid;', [postId]);
     const comments = result.rows;
+    await client.query('COMMIT'); // Commit transaction
     event.reply('comments', comments);
-    client.release();
   } catch (err) {
+    await client.query('ROLLBACK'); // Rollback transaction on error
     console.error('Error executing query', err);
     event.reply('comments', []);
+  } finally {
+    client.release();
   }
 });
 
 // SELECT all Groups that a User is not a Member in
 ipcMain.on('getGroupsByUser', async (event, userId) => {
+  const client = await pool.connect();
   try {
-    const client = await pool.connect();
+    await client.query('BEGIN'); // Start transaction
     const result = await client.query(`
         SELECT $1 AS userID, g.groupID, g.name
         FROM "group" g
@@ -190,41 +183,50 @@ ipcMain.on('getGroupsByUser', async (event, userId) => {
         WHERE m.userID IS NULL;;
         `, [userId]);
     const groups = result.rows;
+    await client.query('COMMIT'); // Commit transaction
     event.reply('groupByUser', groups); // Sending the data back to the renderer process
-    client.release();
   } catch (err) {
+    await client.query('ROLLBACK'); // Rollback transaction on error
     console.error('Error executing query', err);
     event.reply('groupByUser', []); // Sending an empty array in case of error
+  } finally {
+    client.release();
   }
 });
 
 // INSERT a user to a new group in Membership table
 ipcMain.on('joinGroup', async (event, groupId, userId) => {
+  const client = await pool.connect();
   try {
-    // Get the current daate format (YYYY-MM-DD)
+    // Get the current date format (YYYY-MM-DD)
     const currentDate = new Date().toISOString().split('T')[0];
-    const client = await pool.connect();
+    await client.query('BEGIN'); // Start transaction
     await client.query('INSERT INTO membership (userID, groupID, joinDate) VALUES ($1, $2, $3);', [userId, groupId, currentDate]);
+    await client.query('COMMIT'); // Commit transaction
     event.reply('joinGroupResult', true); // Sending success response
-    client.release();
   } catch (err) {
+    await client.query('ROLLBACK'); // Rollback transaction on error
     console.error('Error executing query', err);
     event.reply('joinGroupResult', false); // Sending error response
+  } finally {
+    client.release();
   }
 });
 
 // DELETE a comment from a group
 ipcMain.on('deleteComment', async (event, commentID) => {
+  const client = await pool.connect();
   try {
-    const client = await pool.connect();
+    await client.query('BEGIN'); // Start transaction
     await client.query('DELETE FROM comment WHERE commentID = $1;', [commentID]);
+    await client.query('COMMIT'); // Commit transaction
     event.reply('deleteCommentResult', true); // Sending success response
-    // TODO: Delete for packaging
-    console.log('comment deleted');
-    client.release();
   } catch (err) {
+    await client.query('ROLLBACK'); // Rollback transaction on error
     console.error('Error executing query', err);
     event.reply('deleteCommentResult', false); // Sending error response
+  } finally {
+    client.release();
   }
 });
 
@@ -251,16 +253,19 @@ ipcMain.on('openDialog', async (event, messageOptions) => {
 
 // SELECT all Group name except for one groupid
 ipcMain.on('getGroupName', async (event, groupID) => {
+  const client = await pool.connect();
   try {
-    const client = await pool.connect();
+    await client.query('BEGIN'); // Start transaction
     const result = await client.query('SELECT name FROM "group" WHERE groupID <> $1;', [groupID]);
     const groupNames = result.rows;
+    await client.query('COMMIT'); // Commit transaction
     event.reply('groupNames', groupNames); // Sending the data back to the renderer process
-    client.release();
-    console.log(groupNames);
   } catch (err) {
+    await client.query('ROLLBACK'); // Rollback transaction on error
     console.error('Error executing query', err);
     event.reply('groupNames', []); // Sending an empty array in case of error
+  } finally {
+    client.release();
   }
 });
 
@@ -284,21 +289,22 @@ function sanitizeInput (dirtyInput) {
 // UPDATE group function
 // Final Handler to UPDATE Group
 ipcMain.on('updateGroup', async (event, updateGroup) => {
+  const client = await pool.connect();
   try {
     // Sanitize input to prevent SQL injection and XSS injection
     const cleanName = sanitizeInput(updateGroup.name);
     const cleanDescription = sanitizeInput(updateGroup.description);
-    console.log(cleanName);
-    console.log(cleanDescription);
     // Perform database update with sanitized input
-    const client = await pool.connect();
+    await client.query('BEGIN'); // Start transaction
     await client.query('UPDATE "group" SET name = $1, description = $2 WHERE groupID = $3;', [cleanName, cleanDescription, updateGroup.groupid]);
+    await client.query('COMMIT'); // Commit transaction
     event.reply('groupUpdated', true); // Sending success response
-    console.log('Update finished');
-    client.release();
   } catch (err) {
+    await client.query('ROLLBACK'); // Rollback transaction on error
     console.error('Error executing query', err);
     event.reply('groupUpdated', false);
+  } finally {
+    client.release();
   }
 });
 // Database change watching
@@ -313,7 +319,6 @@ pool.connect((err, client, done) => {
   client.query('LISTEN changes');
 });
 
-// TODO: Add confirm message box before closing
 // Quit the app when all windows are closed (Windows & Linux)
 app.on('window-all-closed', () => {
   if (!isMac) {
